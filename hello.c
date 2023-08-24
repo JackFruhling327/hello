c3ed95c9591b4ecdf809153853eb3b6da7593b8c
Dade Lenox <dlenox2@huskers.unl.edu>
2023-08-24 13:14:50 -0500

    initial commit

--- hello.c
+++ hello.c
@@ -1,2 +1,18 @@
-#include <stdio.h>
+/**
+ * Author: Dade Lenox
+ *
+ * Date: 2023-08-24
+ *
+ * A simple hello world program in C
+ *
+ */
 #include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello World!\n");
+  printf("I love the School of Computing!\n");
+
+  return 0;
+}
